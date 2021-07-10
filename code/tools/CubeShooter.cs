bd404421f0217d7482c8a39af59ba6f249175f76
mancubminecraft <mancubearley@gmail.com>
2021-07-10 11:26:01 -0400

    cubeshooter

---
+++ code/tools/CubeShooter.cs
@@ -0,0 +1,38 @@
+﻿namespace Sandbox.Tools
+{
+	[Library( "tool_cubegun", Title = "Aperture Science Weighted Companion Cube Shooter", Description = "ASWCCS Will generate any number of Companions for you price free! (Do not let it get close to fire)", Group = "fun" )]
+	public class CubeShooter : BaseTool
+	{
+		TimeSince timeSinceShoot;
+
+		public override void Simulate()
+		{
+			if ( Host.IsServer )
+			{
+				if ( Input.Pressed( InputButton.Attack1 ) )
+				{
+					ShootBox();
+				}
+
+				if ( Input.Down( InputButton.Attack2 ) && timeSinceShoot > 0.05f )
+				{
+					timeSinceShoot = 0;
+					ShootBox();
+				}
+			}
+		}
+
+		void ShootBox()
+		{
+			var ent = new Prop
+			{
+				Position = Owner.EyePos + Owner.EyeRot.Forward * 50,
+				Rotation = Owner.EyeRot
+			};
+
+			ent.SetModel( "models/citizen_props/crate01.vmdl" );
+			ent.Velocity = Owner.EyeRot.Forward * 1000;
+		}
+	}
+
+}
