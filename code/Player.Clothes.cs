309535db2e7536a8040414b0bd11d78ea0d2423b
GmanChewing-HLAModder <76550801+GmanChewing-HLAModder@users.noreply.github.com>
2021-07-29 20:52:00 -0400

    s&box cap debug spawn test

--- code/Player.Clothes.cs
+++ code/Player.Clothes.cs
@@ -86,21 +86,7 @@ partial class SandboxPlayer
 		{
 			var model = Rand.FromArray( new[]
 			{
-				"models/citizen_clothes/hat/hat_hardhat.vmdl",
-				"models/citizen_clothes/hat/hat_woolly.vmdl",
-				"models/citizen_clothes/hat/hat_securityhelmet.vmdl",
-				"models/citizen_clothes/hair/hair_malestyle02.vmdl",
-				"models/citizen_clothes/hair/hair_femalebun.black.vmdl",
-				"models/citizen_clothes/hat/hat_beret.red.vmdl",
-				"models/citizen_clothes/hat/hat.tophat.vmdl",
-				"models/citizen_clothes/hat/hat_beret.black.vmdl",
-				"models/citizen_clothes/hat/hat_cap.vmdl",
-				"models/citizen_clothes/hat/hat_leathercap.vmdl",
-				"models/citizen_clothes/hat/hat_leathercapnobadge.vmdl",
-				"models/citizen_clothes/hat/hat_securityhelmetnostrap.vmdl",
-				"models/citizen_clothes/hat/hat_service.vmdl",
-				"models/citizen_clothes/hat/hat_uniform.police.vmdl",
-				"models/citizen_clothes/hat/hat_woollybobble.vmdl",
+				"models/sboxhardhatbase.vmdl",
 			} );
 
 			hat = new ModelEntity();
