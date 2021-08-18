23189a8b1d3f6c03002474817fc06d6192dad5ec
GmanChewing-HLAModder <76550801+GmanChewing-HLAModder@users.noreply.github.com>
2021-08-18 04:07:15 -0400

    portal radio finally works

---
+++ code/entities/Radio.cs
@@ -0,0 +1,50 @@
+﻿using Sandbox;
+using System;
+
+[Library( "ent_radio", Title = "Radio", Spawnable = true )]
+public partial class RadioEntity : Prop, IUse
+{
+
+	public bool Enable { get; set; } = false;
+	private Sound RadioSnd;
+
+	public override void Spawn()
+	{
+		base.Spawn();
+		CheckSnd();
+		SetModel("models/props/portal/radio_remade.vmdl");
+		SetupPhysicsFromModel( PhysicsMotionType.Dynamic, false );
+	}
+
+	public bool IsUsable( Entity user )
+	{
+		return true;
+	}
+
+	public bool OnUse( Entity user )
+	{
+		if ( user is Player player )
+		{
+			CheckSnd();
+		}
+
+		return false;
+	}
+
+	public void CheckSnd()
+	{
+		if(Enable==true){
+			Enable=false;
+			RadioSnd = base.PlaySound("radioloop");
+		}else{
+			Enable=true;
+			RadioSnd.Stop();
+		}
+	}
+
+	protected override void OnDestroy()
+	{
+		RadioSnd.Stop();
+		base.OnDestroy();
+	}
+}
---
+++ code/ui/Scoreboard.cs
@@ -0,0 +1,71 @@
+﻿
+using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+public class Scoreboard : Sandbox.UI.Scoreboard<ScoreboardEntry>
+{
+
+	public Scoreboard()
+	{
+		StyleSheet.Load( "/ui/Scoreboard.scss" );
+		AddClass( "scoreboard" );
+	}
+
+	protected override void AddHeader()
+	{
+		Header = Add.Panel( "header" );
+		Header.Add.Label( "player", "name" );
+		Header.Add.Label( "deaths", "deaths" );
+		Header.Add.Label( "ping", "ping" );
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		Canvas.SortChildren( c =>
+		{
+			var snd = c.GetChild( 2 );
+			var l = (snd as Label);
+			int.TryParse( l?.Text ?? "0", out int rank );
+			return -rank;
+		} );
+	}
+
+	protected override void AddPlayer( PlayerScore.Entry entry )
+	{
+		base.AddPlayer( entry );
+
+	}
+}
+
+public class ScoreboardEntry : Sandbox.UI.ScoreboardEntry
+{
+	public Label Rank;
+	public Image Avatar;
+	public Panel Color;
+	public ScoreboardEntry()
+	{
+		PlayerName.Delete();
+		Kills.Delete();
+		Deaths.Delete();
+		Ping.Delete();
+
+		Color = Add.Panel( "color" );
+		Avatar = Add.Image( "avatar:76561198099710884", "avatar" );
+		PlayerName = Add.Label( "Player Name", "name" );
+		Deaths = Add.Label( "0", "deaths" );
+		Ping = Add.Label( "0", "ping" );
+	}
+
+	public override void UpdateFrom( PlayerScore.Entry entry )
+	{
+		base.UpdateFrom( entry );
+
+		Color.Style.BackgroundColor = new Color( entry.Get<uint>( "color", 0 ) );
+		Avatar.SetTexture( $"avatar:{entry.Get<ulong>( "steamid", 0 )}" );
+
+		SetClass( "me", Local.Client != null && entry.Get<ulong>( "steamid", 0 ) == Local.Client.SteamId );
+	}
+}
