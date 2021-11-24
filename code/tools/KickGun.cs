1b860f9c8e7a3e47259320a81326b84fccab673b
Ian Earley <gmanchewing@gmail.com>
2021-11-23 19:39:32 -0500

    added clothing test

---
+++ code/tools/KickGun.cs
@@ -0,0 +1,35 @@
+﻿namespace Sandbox.Tools
+{
+	public partial class PlayerRemoverTool : BaseTool
+	{
+		public override void Simulate()
+		{
+			if ( !Host.IsServer )
+				return;
+
+			using ( Prediction.Off() )
+			{
+				if ( !Input.Pressed( InputButton.Attack1 ) )
+					return;
+
+				var startPos = Owner.EyePos;
+				var dir = Owner.EyeRot.Forward;
+
+				var tr = Trace.Ray( startPos, startPos + dir * MaxTraceDistance )
+					.Ignore( Owner )
+					.HitLayer( CollisionLayer.Debris )
+					.Run();
+
+				if ( tr.Entity is not Player )
+					return;
+
+				tr.Entity.Client.Kick();
+
+				CreateHitEffects( tr.EndPos );
+
+				var particle = Particles.Create( "particles/physgun_freeze.vpcf" );
+				particle.SetPosition( 0, tr.Entity.Position );
+			}
+		}
+	}
+}
